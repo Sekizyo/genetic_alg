015b324e61f7fdc026d0371a1433b9be2d636d9e
Mateusz Pieczulis <mateuszpieczulis123@gmail.com>
2024-10-28 11:10:37 +0100

    split to classes

--- main.py
+++ main.py
@@ -1,45 +1,114 @@
 import math
-from random import uniform
-import tkinter as tk
-from tkinter import ttk, Tk, Label, Entry
+import numpy as np
+from random import random, uniform
+# import tkinter as tk
+# from tkinter import ttk, Tk, Label, Entry
 
 class Individual():
-    def __init__(self, id: int, x_real: float) -> None:
-        self.id = id
-        self.x_real = x_real
-        self.fx = 0
-        self.gx = 0
-        self.p = 0
-        self.q = 0
-        self.r = 0
-        self.new_x = 0
+    def __init__(self, id: int) -> None:
+        self.id = id               # Unique identifier for the individual
+        self.x_real = 0       # The real value of x (within the range [a, b])
+        self.x_int = 0
+        self.fx = 0                # Fitness value (calculated using F(x))
+        self.x_bin = ""           # Binary representation of the integer x
+        self.new_x = 0             # New value of x after selection, crossover, and mutation
+        
+    def set_random_x(self, a: int, b: int, roundTo: float) -> None:
+        self.x_real = round(uniform(a, b), roundTo)    
+        return self.x_real
+        
+    def get_x_real(self, x: float, a: int, b: int, l: int, d: int) -> float:
+        return round(((x*(b-a))/(2**l-1))+a, d)
+    
+    def set_x_int(self, a: int, b: int, l: int) -> int:
+        self.x_int = math.ceil((1/(b-a))*(self.x_real - a)*(2**l - 1))
+        return self.x_int
+    
+    def get_bin_int(self, x: float) -> int:
+        return int(x, 2)
 
+    def set_x_bin(self, l: int):
+        return format(self.x_int, f'0{l}b')
+
+    def set_fitness(self) -> float:
+        self.fx = (self.x_int % 1) * (math.cos(20 * math.pi * self.x_int) - math.sin(self.x_int))
+        return self.fx
+    
+    def mutate(self, p: float) -> int:
+        mutated_count = 0
+        genes = list(self.x_bin)
+        for i, gene in enumerate(genes):
+            if p >= random():
+                mutated_count += 1
+                if gene == "1":
+                    genes[i] = "0"
+                else:
+                    genes[i] = "1"
+                    
+        self.x_bin = "".join(genes)
+        
+        return mutated_count
+    
+    def print_values(self) -> list[int | float | str]:
+        return [self.x_real, self.fx, self.gx, self.p, self.q, self.r]
+    
+class Symulation():
+    def __init__(self, a: int, b: int, n: int, d: float, roundTo: int, pk: float, pm: float) -> None:
+        self.a = a
+        self.b = b
+        self.d = d
+        self.roundTo = roundTo
+        self.population_size = n
+        self.binSize = self.get_bin_size(self.a, self.b, d)
+        self.p = pk
+        self.q = 0
+        self.pm = pm
+    
+    def get_bin_size(self, a: int, b: int, d: int) -> int:
+        return math.ceil(math.log(((b-a)/d)+1, 2))
+        
+    def create_population(self) -> list[Individual]:
+        population = []
+        for i in range(self.population_size):
+            individual = Individual(i)
+            individual.set_random_x(self.a, self.b, self.roundTo)
+            individual.set_x_int(self.a, self.b, self.binSize)
+            individual.set_x_bin(self.binSize)
+            individual.set_fitness()
+            print(vars(individual))
+            
+            population.append(individual)
+            
+        return population
+    
+    def selection(self, population: list[Individual]) -> list[Individual]:
+        total_fitness = 0
+        probabilities = []
+        for invidual in population:
+            total_fitness += invidual.fx
+            
+        for invidual in population:
+            probabilities.append(invidual.fx/total_fitness)
+            
+        selected_population = np.random.choice(population, size=len(population), p=probabilities, replace=True)
+        
+        return selected_population
+    
+    def mutation(self, population: list[Individual]) -> list[Individual]:
+        mutations = []
+        for invidual in population:
+            mutation = invidual.mutate(self.p)
+            if mutation > 0:
+                mutations.append(mutation)
+                
+        return population, mutations
+            
+            
 class Window():
     def __init__(self) -> None:
         self.root = Tk()
         self.root.geometry("1000x500")
         self.root.title("Algorytm genetyczny - Matas Pieczulis 21162")
-
-    def get_x(self, start: int, stop: int, step: float) -> float:
-        return round(uniform(start, stop), step)
-
-    def F(self, x: float, d: int):
-        return (x % 1) * (math.cos(20 * math.pi * x) - math.sin(x))
-
-    def x_real(self, x: float, a: int, b: int, l: int, d: int) -> float:
-        return round(((x*(b-a))/(2**l-1))+a, d)
-    
-    def x_int(self, x: float, a: int, b: int, l: int) -> int:
-        return math.ceil((1/(b-a))*(x - a)*(2**l - 1))
-    
-    def get_l(self, a: int, b: int, d: int) -> int:
-        return math.ceil(math.log(((b-a)/d)+1, 2))
-    
-    def int_bin(self, x: float, l: int):
-        return format(x, f'0{l}b')
-    
-    def bin_int(self, x: float) -> int:
-        return int(x, 2)
 
     def get_data(self) -> list[int]:
         _map = {
@@ -55,30 +124,13 @@ class Window():
             d = self.d_box.get()
             pk = float(self.pk_entry.get())
             pm = float(self.pm_entry.get())
-            l = self.get_l(a, b, float(d))
         except:
-            return (-4, 12, 10, 2, 13, 0, 0)
-        return (a, b, n, _map[d], l, pk, pm)
-
-    def process_data(self, data: list[int]) -> list[int]:
-        a, b, n, d, l, pk, pm = data 
-        output = []
-        for i in range(1, n+1):
-            x = self.get_x(a, b, d)
-            x_int1 = self.x_int(x, a, b, l)
-            x_bin = self.int_bin(x_int1, l)
-            x_int2 = self.bin_int(x_bin)
-            x_real2 = self.x_real(x_int2, a, b, l, d)
-
-            func = self.F(x_real2, d)
-
-            line = [i, x, x_int1, x_bin, x_int2, x_real2, func]
-            output.append(line)
-
-        return output
+            return (-4, 12, 10, 0.01, 2, 0.001, 0.001)
+        return (-4, 12, 10, 0.01, 2, 0.001, 0.001)
+        return (a, b, n, d, _map[d], pk, pm)
 
     def plot_table(self, data: list[int]) -> None:
-        columns = ["LP", "x_real", "x_int", "x_bin", "x_int2", "x_real2", "f(x)"]
+        columns = ["LP", "x_real", "f(x)", "g(x)", "p", "q", "r", "x_real", "x bin", "parents", "children", "population","mutation point", "mutation bin", "mutation real", "mutation f(x)"]
         
         tree = ttk.Treeview(self.root, columns=columns, show="headings", height=10)
 
@@ -99,9 +151,12 @@ class Window():
         hsb.grid(row=11, column=0, columnspan=6, sticky='ew')
 
     def calc(self) -> None:
-        data = self.get_data()
-        processedData = self.process_data(data)
-        self.plot_table(processedData)
+        a, b, n, d, roundTo, pk, pm = self.get_data()
+        self.symulation = Symulation(a, b, n, d, roundTo, pk, pm)
+        population = self.symulation.create_population()
+        population = self.symulation.selection(population)
+        population = self.symulation.mutation(population)
+        # self.plot_table(processedData)
         return
 
     def draw(self) -> None:
@@ -139,7 +194,11 @@ class Window():
         calc_button.grid(column=2, row=8)
         self.root.mainloop()
 
-
-if __name__ == "__main__":
-    win = Window()
-    win.draw()
+# if __name__ == "__main__":
+    # win = Window()
+    # win.draw()
+symulation = Symulation(-4, 12, 10, 0.01, 2, 0.001, 0.5)
+print(vars(symulation))
+population = symulation.create_population()
+population = symulation.selection(population)
+population = symulation.mutation(population)
